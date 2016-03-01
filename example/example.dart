81b1bc5549f599eabff5a63ed40251be0f143b7d
Oliver Damm <oliver.damm@gmx.de>
2016-03-01 18:22:36 +0100

    added comments to the example

--- example/example.dart
+++ example/example.dart
@@ -5,41 +5,54 @@ ShaderProgram program;
 Buffer buffer;
 
 main() {
+  // query the canvas element
   var canvasElement = querySelector('#webgl-canvas');
 
+  // init the webgl rendering context
   glInit(canvasElement);
+
+  // set the color used to clear the color buffer
   glClearColor(0, 0, 0, 1);
 
+  // enable blending and set the blend function
   glEnableBlend(true);
   glBlendFunc(BlendingCoefficient.SRC_ALPHA, BlendingCoefficient.ONE_MINUS_SRC_ALPHA);
 
+  // create a shader program
   program = new ShaderProgram.fromSources(vertexShader, fragmentShader);
 
+  // create a buffer that contains 2 types of per vertex data: positions and colors
   buffer = new Buffer(BufferTarget.ARRAY_BUFFER);
   buffer.data({
     'a_position': {
-      Buffer.SIZE: 2,
+      Buffer.SIZE: 2, // 2 components per vertex (x, y)
       Buffer.DATA: [1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0]
     },
     'a_color': {
-      Buffer.SIZE: 3,
+      Buffer.SIZE: 3, // 3 components per vertex (r, g, b)
       Buffer.DATA: [1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0]
     }
   });
 
+  // start rendering
   window.requestAnimationFrame(render);
 }
 
-render(int tick) {
+render(tick) {
+  // clear the color buffer
   glClear([ClearMode.COLOR_BUFFER]);
 
+  // draw the vertices using the given shader program and the vertex data stored in the buffer. additionally, set an
+  // uniform.
   gluDrawArrays(program, buffer, uniformSetters: {
     'u_alpha': (uniform) => uniform.set1f(0.5)
   });
 
+  // next frame
   window.requestAnimationFrame(render);
 }
 
+// the vertex shader...
 final vertexShader = '''
 #version 100
 
@@ -55,6 +68,7 @@ void main() {
 }
 ''';
 
+// ...and the fragment shader
 final fragmentShader = '''
 #version 100
 
